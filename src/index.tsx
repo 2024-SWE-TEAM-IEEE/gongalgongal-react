e390c2484654d6e489ea5b9daa225cf26e54adba
DGUHJH <ghkdwogud852@gmail.com>
2024-11-05 15:21:45 +0900

    style : global.css 적용

--- src/index.tsx
+++ src/index.tsx
@@ -12,6 +12,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import styled from 'styled-components'
+import './styles/global.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as any)
 
