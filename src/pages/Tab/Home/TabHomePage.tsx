178853d3c1a146cb15ee5970de0e558d426b15fb
DGUHJH <ghkdwogud852@gmail.com>
2024-11-06 20:50:44 +0900

    feat : TabHomePage FilterContainer 추가

--- src/pages/Tab/Home/TabHomePage.tsx
+++ src/pages/Tab/Home/TabHomePage.tsx
@@ -1,7 +1,7 @@
 import { Header } from 'components/Header'
 import { FC } from 'react'
 import { TabBar } from '../components/TabBar'
-import { Root } from './styled'
+import { FilterChip, FilterChipButton, FilterChipContainer, FilterChipTypo, FilterContainer, Root } from './styled'
 
 type TabHomePageProps = {
   className?: string
@@ -11,6 +11,17 @@ export const TabHomePage: FC<TabHomePageProps> = ({ className }) => {
   return (
     <Root className={className}>
       <Header />
+      <FilterContainer>
+        <FilterChipContainer>
+          <FilterChip>
+            <FilterChipTypo>전공 관련 공지</FilterChipTypo>
+          </FilterChip>
+          <FilterChip>
+            <FilterChipTypo>장학금 공지</FilterChipTypo>
+          </FilterChip>
+        </FilterChipContainer>
+        <FilterChipButton>필터 수정</FilterChipButton>
+      </FilterContainer>
       <TabBar />
     </Root>
   )
--- src/pages/Tab/Home/styled.ts
+++ src/pages/Tab/Home/styled.ts
@@ -1,3 +1,46 @@
+import { Button, Typography } from 'antd'
+import { HEADER_HEIGHT, TAB_BAR_HEIGHT } from 'constants/layout'
 import styled from 'styled-components'
 
-export const Root = styled.div``
+export const Root = styled.div`
+  min-height: 100vh;
+  background: #f5f5f5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: ${HEADER_HEIGHT}px;
+  padding-bottom: ${TAB_BAR_HEIGHT}px;
+`
+
+export const FilterContainer = styled.div`
+  width: calc(100% - 40px);
+  background: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px 20px;
+  box-sizing: border-box;
+  border-radius: 8px;
+  margin-top: 20px;
+`
+
+export const FilterChipContainer = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`
+
+export const FilterChip = styled.div`
+  background: #eee;
+  padding: 4px 12px;
+  box-sizing: border-box;
+  border-radius: 12px;
+`
+
+export const FilterChipTypo = styled(Typography)`
+  font-size: 14px;
+  font-weight: 500;
+  color: #333;
+`
+
+export const FilterChipButton = styled(Button)``
